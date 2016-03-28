ef4ecef36b7aeb05eedb5ff33d4de56bfb8da96b
Philip Steiner <philip@psteiner.com>
2016-03-27 18:44:52 -0700

    First commit

---
+++ bin/indeedy.pl
@@ -0,0 +1,20 @@
+#!perl
+
+use strict;
+use warnings;
+use 5.10.0;
+
+use Indeedy::Fetcher;
+Indeedy::Fetcher->run();
+
+__END__
+
+=head1 NAME
+
+indeedy-fetcher
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
---
+++ lib/Indeedy/Fetcher.pm
@@ -0,0 +1,11 @@
+package Indeedy::Fetcher;
+
+use warnings;
+use strict;
+
+sub run
+{
+    print "Fetching Jobs...\n";
+}
+
+1;
